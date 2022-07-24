459753f558e33e94b7eed61d18cb8ba2ed1c394c
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-07-25 00:14:36 +0100

    Set error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
+const errorHandler = require('./middleware/errorMiddleware');
+
 const port = process.env.PORT;
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
