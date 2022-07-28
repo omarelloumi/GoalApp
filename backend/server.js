924dc3194404942e73a09bd043b70b1e7d04344a
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-07-28 06:54:22 +0100

    deployment preparation

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,21 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
+
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 }
--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -8,7 +8,7 @@ function GoalItem({ goal }) {
     <div className='goal'>
       <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
       <h2>{goal.text}</h2>
-      <button onClick={() => { dispatch(deleteGoal(goal.id)) }} className='close'>
+      <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
         X
       </button>
     </div>
